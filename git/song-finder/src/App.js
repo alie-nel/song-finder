32df031b3b6dcc4cb973b046625fafd6e20ec982
alie-nel <msnguyen63@gmail.com>
2024-01-13 17:45:28 +1100

    set up repository

---
+++ git/song-finder/src/App.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import { db } from './config/firebase';
+
+function App() {
+  
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
